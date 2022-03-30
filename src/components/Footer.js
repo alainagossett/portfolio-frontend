8e31a426829605bad5a81a1201f6fde2b61798a8
Alaina <alaina.gossett@gmail.com>
2022-03-29 18:51:41 -0600

    initializes app

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+    return <h1>Alaina Gossett &copy; 2022</h1>
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <h1>Alaina Gossett Portfolio</h1>
+}
+
+export default Header;
