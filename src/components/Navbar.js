43c6d2bb43ac06178b0b660d510f206e329d4d38
Alaina <alaina.gossett@gmail.com>
2022-03-30 19:06:19 -0600

    fixes some links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-1 md:border-gray-700 flex flex-wrap items-center text-base justify-center">
                     <Link to="/projects" className="mr-5 hover:text-white">
-                        Projects
+                        Portfolio
                     </Link>
                     <Link to="/about" className="mr-5 hover:text-white">
                         About
