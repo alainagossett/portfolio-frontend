136c395f7cc73b8ada444cc7003ac6b645543df3
Alaina <alaina.gossett@gmail.com>
2022-03-30 19:48:52 -0600

    adds About content

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,26 +8,22 @@ function Landing(props) {
                 className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                     Alaina Gossett
-                    <br className="hidden lg:inline-block" /> This is my portfolio
                 </h1>
+                <h2 className='title-font sm:text-2xl text-1xl mb-2 font-medium text-white'>
+                    Hi, I'm so glad you're here
+                </h2>
                 <p className="mb-8 leading-relaxed">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                    laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                    Laborum, voluptas natus?
+                    Welcome to my portfolio site! I'd like to showcase some recent projects I've worked on as well as give you a better idea of who I am as a Software Engineer. 
                 </p>
                 <div className="flex justify-center">
                     <Link href="/contact"
                         className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                         Contact
                     </Link>
-                    {/* <a href="#projects"
-                        className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                        Projects
-                    </a> */}
                 </div>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <img src="" alt="Alaina Gossett" className="object-cover object-center rounded" />
+                <img src="https://i.imgur.com/TpIpPKu.jpg" alt="Alaina Gossett" className="object-cover object-center rounded" />
             </div>
         </div>
     </section>
